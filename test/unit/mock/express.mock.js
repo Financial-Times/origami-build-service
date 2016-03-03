dcc1f4848af50b2bb3e3ce6505a30a3d3706d2d7
Rowan Manning <rowan.manning@ft.com>
2016-03-03 11:00:51 +0000

    Move the redirectWithBody function into its own module

--- test/unit/mock/express.mock.js
+++ test/unit/mock/express.mock.js
@@ -4,5 +4,16 @@ const sinon = require('sinon');
 
 module.exports = sinon.stub().returns({
 	disable: sinon.spy(),
-	use: sinon.spy()
+	use: sinon.spy(),
 });
+
+module.exports.createMockResponse = function() {
+	const response = {
+		location: sinon.stub(),
+		send: sinon.stub(),
+		status: sinon.stub()
+	};
+	response.location.returns(response);
+	response.status.returns(response);
+	return response;
+};
