3d8ed7084d959f3deaff2ff4cd8bfd25116463d3
Rowan Manning <rowan.manning@ft.com>
2016-04-27 10:59:49 +0100

    Tweak the messaging for deprecated v1 redirects

--- deprecated/v1-deprecated.js
+++ deprecated/v1-deprecated.js
@@ -6,7 +6,7 @@ const URL = require('url');
 
 module.exports = function(req, res) {
 	const redirectUrl = '/v2' + URL.parse(req.url).path.replace(/^\/v1/, '');
-	const redirectBody = 'This endpoint has been deprecated. You are being redirected to ' + redirectUrl + '\nSee https://' + hostnames.preferred + '/v2/#api-reference for more information.\n';
+	const redirectBody = 'This endpoint has been removed. You are being redirected to ' + redirectUrl + '\nSee https://' + hostnames.preferred + '/v2/#api-reference for more information.\n';
 	res.type('txt');
 	res.header('Access-Control-Allow-Origin', '*');
 	redirectWithBody(res, 301, redirectUrl, redirectBody);
