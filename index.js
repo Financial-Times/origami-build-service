ba5de4196100389461f64a04cae3511577c7f7df
Rowan Manning <rowan.manning@ft.com>
2016-08-15 10:42:51 +0100

    Correct the panic guides for memory/disk issues

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ const buildSystem = new BuildSystem({
 		severity: 1,
 		checkPeriod: 120,
 		technicalSummary: 'Process has run out of available memory',
-		panicGuide: 'Restart the service on `' + hostname + '`, Try: `ssh ' + hostname + '`, then `sudo service buildservice restart` and re-check health status for `' + hostname + ':8080/__health`'
+		panicGuide: 'Restart the service using the `heroku` command line tool: `heroku restart --app origami-buildservice-eu`.'
 	});
 
 	dirInitialised.then(function() {
@@ -149,7 +149,7 @@ const buildSystem = new BuildSystem({
 			severity: 1,
 			checkPeriod: 120,
 			technicalSummary: '/tmp directory is full, new modules will not be installable.',
-			panicGuide: 'Restart the service on `' + hostname + '`, Try: `ssh ' + hostname + '`, then `sudo service buildservice restart` and re-check health status for `' + hostname + ':8080/__health`',
+			panicGuide: 'Restart the service using the `heroku` command line tool: `heroku restart --app origami-buildservice-eu`.',
 			businessImpact: 'New modules will not be able to install and existing modules will not refresh.  As problem persists expect end user reports from critical sites regarding styling and broken functionality.'
 		}, tempdir);
 	});
