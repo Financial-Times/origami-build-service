d37e9f42d413b1dcdc7c6a752e2abc0f302c8ce1
Jake Champion <jakechampion.jake2@googlemail.com>
2017-02-02 18:04:33 +0000

    Remove env var which seems to do nothing to test results

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -8,13 +8,6 @@ const uuid = require('uuid');
 const tempdir = `/tmp/buildservice/tests-${uuid()}`;
 
 before(function() {
-
-	// This overrides the Bower shorthand resolve, which allows us to
-	// refer to `mock-modules/<name>` in all of the tests. Bower will
-	// look on the local file-system instead of trying to load modules
-	// from GitHub or the registry.
-	process.env.bower_shorthand_resolver = `${__dirname}/{{shorthand}}`;
-
 	mkdirp.sync(tempdir);
 
 	this.app = buildService({
