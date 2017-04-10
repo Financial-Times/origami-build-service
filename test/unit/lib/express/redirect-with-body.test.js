eafa171a99903075105df04607382660c8197b84
Rowan Manning <accounts@rowanmanning.co.uk>
2017-04-10 15:41:44 +0100

    Remove custom metrics code and use Origami Service (#123)
    * Remove custom metrics code and use Origami Service
    
    * Fix an issue with Object.assign

--- test/unit/lib/express/redirect-with-body.test.js
+++ test/unit/lib/express/redirect-with-body.test.js
@@ -3,11 +3,11 @@
 const assert = require('chai').assert;
 
 describe('lib/express/redirect-with-body', function() {
-	let express;
+	let origamiService;
 	let redirectWithBody;
 
 	beforeEach(function() {
-		express = require('../../mock/express.mock');
+		origamiService = require('../../mock/origami-service.mock');
 		redirectWithBody = require('../../../../lib/express/redirect-with-body');
 	});
 
@@ -24,22 +24,22 @@ describe('lib/express/redirect-with-body', function() {
 			status = 301;
 			location = 'foo';
 			body = 'bar';
-			redirectWithBody(express.mockResponse, status, location, body);
+			redirectWithBody(origamiService.mockResponse, status, location, body);
 		});
 
 		it('should set the response status', function() {
-			assert.calledOnce(express.mockResponse.status);
-			assert.calledWithExactly(express.mockResponse.status, status);
+			assert.calledOnce(origamiService.mockResponse.status);
+			assert.calledWithExactly(origamiService.mockResponse.status, status);
 		});
 
 		it('should set the response location', function() {
-			assert.calledOnce(express.mockResponse.location);
-			assert.calledWithExactly(express.mockResponse.location, location);
+			assert.calledOnce(origamiService.mockResponse.location);
+			assert.calledWithExactly(origamiService.mockResponse.location, location);
 		});
 
 		it('should send the response body', function() {
-			assert.calledOnce(express.mockResponse.send);
-			assert.calledWithExactly(express.mockResponse.send, body);
+			assert.calledOnce(origamiService.mockResponse.send);
+			assert.calledWithExactly(origamiService.mockResponse.send, body);
 		});
 
 	});
