7ca0afdba37c52d8a2e270357107962a459c43f5
Charlie Briggs <charliebriggs@gmail.com>
2017-11-13 12:06:04 +0000

    Add surrogate cache control header with 1 year TTL to deprecated routes (#152)
    This affects 301 redirect response

--- deprecated/v1-deprecated.js
+++ deprecated/v1-deprecated.js
@@ -4,11 +4,14 @@ const hostnames = require('../lib/utils/hostnames');
 const redirectWithBody = require('../lib/express/redirect-with-body');
 const URL = require('url');
 
+const ONE_YEAR_SECONDS = 31536000;
+
 module.exports = function(req, res) {
 	const requestPath = URL.parse(req.url).path;
 	const redirectUrl = requestPath.replace(new RegExp('^/(v1/)?'), req.basePath + 'v2/');
 	const redirectBody = 'This endpoint has been removed. You are being redirected to ' + redirectUrl + '\nSee https://' + hostnames.preferred + '/v2/#api-reference for more information.\n';
 	res.type('txt');
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Surrogate-Control', `public, max-age=${ONE_YEAR_SECONDS}, stale-while-revalidate=${ONE_YEAR_SECONDS}, stale-if-error=${ONE_YEAR_SECONDS}`);
 	redirectWithBody(res, 301, redirectUrl, redirectBody);
 };
