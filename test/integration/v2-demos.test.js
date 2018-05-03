e46edf30715c4499689f056a68189c7edd797574
Jake Champion <jakechampion.jake2@googlemail.com>
2018-05-03 16:57:45 +0100

    Add brand to the opts used when bundling

--- test/integration/v2-demos.test.js
+++ test/integration/v2-demos.test.js
@@ -111,6 +111,30 @@ describe('GET /v2/demos', function() {
 
 	});
 
+	describe('when a valid module at specific version and demo and brand are requested', function() {
+		const moduleName = 'o-test-component@1.0.19';
+		const pathName = 'main';
+
+		beforeEach(function() {
+			this.request = request(this.app)
+				.get(`/v2/demos/${moduleName}/${pathName}?brand=internal`)
+				.set('Connection', 'close');
+		});
+
+		it('should respond with a 200 status', function(done) {
+			this.request.expect(200).end(done);
+		});
+
+		it('should respond with the expected `Content-Type` header', function(done) {
+			this.request.expect('Content-Type', 'text/html;charset=UTF-8').end(done);
+		});
+
+		it('should respond with the file contents', function(done) {
+			this.request.expect('<!DOCTYPE html>\n<html lang="en" class="o-hoverable-on ">\n<head>\n\t<meta charset="utf-8">\n\t<meta http-equiv="X-UA-Compatible" content="IE=Edge">\n\t<title>test-component: main demo</title>\n\t<meta name="viewport" content="initial-scale=1.0, width=device-width">\n\t<script src="//cdn.polyfill.io/v2/polyfill.min.js?features=default"></script>\n\t<style>body { margin: 0; } .core .o--if-js, .enhanced .o--if-no-js { display: none !important; }</style>\n\t<script>(function(d) { d.className = d.className + \' demo-js\'; })(document.documentElement);</script>\n\t<link rel="stylesheet" href="//www.ft.com/__origami/service/build/v2/bundles/css?modules=&amp;brand=internal">\n</head>\n<body>\n<div>\n</div>\n\n<script src="//www.ft.com/__origami/service/build/v2/bundles/js?modules="></script>\n<script src="//registry.origami.ft.com/embedapi?autoload=resize"></script>\n</body>\n</html>\n').end(done);
+		});
+
+	});
+
 	describe('when a valid module at specific version and demo which contains compilation errors are requested', function() {
 		const moduleName = 'o-test-component@1.0.8';
 		const pathName = 'main';
