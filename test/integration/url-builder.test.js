aa23978bfaa5d543ea973af37139f4d42300bab5
notlee <notlee@users.noreply.github.com>
2021-02-09 20:03:37 +0000

    Add URL Builder
    The URL Builder accepts an existing Build Service URL and builds
    an updated URL which includes the latest version number for all
    components, helping users to upgrade their Build Service requests
    and migrate to the new version of components. To further help a
    table is displayed which shows the components which were requested,
    the latest version number for the component, a label to show if it
    is up to date or not, and a link to the readme to access the
    migration guides.
    
    So far it does not handle all cases, e.g:
    - Only the "bundles" endpoint is supported
    - Module filepaths and non-Origami components aren't supported
    - If the latest version of components can only be used with the
    (upcoming) v3 Build Service endpoint then the generated URL will
    include a v3 Build Service url but will not warn about any
    parameters used which are  no longer be supported in v3 of the
    Build Service

---
+++ test/integration/url-builder.test.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const request = require('supertest');
+const proclaim = require('proclaim');
+const cheerio = require('cheerio');
+
+describe('GET /url-builder', function () {
+    this.timeout(20000);
+    this.slow(5000);
+
+    const modules = 'o-test-component@^1.0.0';
+
+    beforeEach(function () {
+        this.request = request(this.app)
+            .post(`/url-builder`)
+            .send(`build-service-url=https://www.ft.com/__origami/service/build/v2/bundles/css?modules=${modules}&brand=internal`)
+            .set('Connection', 'close');
+    });
+
+    it('should respond with a 200 status', function (done) {
+        this.request.expect(200).end(done);
+    });
+
+    it('should include a build service url input', function (done) {
+        this.request
+            .expect(({ text }) => {
+                const $ = cheerio.load(text);
+                const element = $('input[name="build-service-url"]');
+                proclaim.ok(element.length, 'Could not find input of name "build-service-url".');
+            })
+            .end(done);
+    });
+});
+
+describe('POST /url-builder', function () {
+    this.timeout(20000);
+    this.slow(5000);
+
+    describe('with a valid build service url', function () {
+        const modules = 'o-test-component@^1.0.0';
+
+        beforeEach(function () {
+            this.request = request(this.app)
+                .post(`/url-builder`)
+                .send(`build-service-url=https://www.ft.com/__origami/service/build/v2/bundles/css?modules=${modules}&brand=internal`)
+                .set('Connection', 'close');
+        });
+
+        it('should respond with a 200 status', function (done) {
+            this.request.expect(200).end(done);
+        });
+
+        it('should respond with an updated build service url', function (done) {
+            // expect a release of v2 or later in the updated url
+            this.request.expect(/modules&#x3D;o-test-component@\^([2-9]|\d\d+)/).end(done);
+        });
+    });
+
+    describe('with a build service url with non-existent module', function () {
+        const modules = 'o-no-i-am-not-real@^1.0.0';
+
+        beforeEach(function () {
+            this.request = request(this.app)
+                .post(`/url-builder`)
+                .send(`build-service-url=https://www.ft.com/__origami/service/build/v2/bundles/css?modules=${modules}&brand=internal`)
+                .set('Connection', 'close');
+        });
+
+        it('should respond with a 400 status', function (done) {
+            this.request.expect(400).end(done);
+        });
+
+        it('should respond with a helpful error message', function (done) {
+            this.request.expect(/Could not find/).end(done);
+        });
+    });
+
+    describe('with a build service url with unexpected characters', function () {
+        const modules = 'o-test-component@^1.0.0,<script></script>';
+
+        beforeEach(function () {
+            this.request = request(this.app)
+                .post(`/url-builder`)
+                .send(`build-service-url=https://www.ft.com/__origami/service/build/v2/bundles/css?modules=${modules}&brand=internal`)
+                .set('Connection', 'close');
+        });
+
+        it('should respond with a 400 status', function (done) {
+            this.request.expect(400).end(done);
+        });
+
+        it('should respond with a helpful error message', function (done) {
+            this.request.expect(/unexpected/).end(done);
+        });
+    });
+});
