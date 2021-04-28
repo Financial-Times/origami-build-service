63fc5ea45833989f62bca8fb6faaf100721f34aa
Jake Champion <me@jakechampion.name>
2021-04-28 11:01:40 +0100

    Update createEntryFileSass.test.js

--- test/unit/lib/middleware/v3/createEntryFileSass.test.js
+++ test/unit/lib/middleware/v3/createEntryFileSass.test.js
@@ -17,6 +17,53 @@ describe('lib/middleware/v3/createEntryFileSass', function() {
 
 		const components = {
 			'@financial-times/o-table': 'prerelease',
+			'@financial-times/o-grid': 'prerelease'
+		};
+
+		await fs.writeFile(path.join(location, 'package.json'), JSON.stringify({
+			dependencies: components
+		}));
+
+		await installDependencies(location);
+
+		const brand = 'master';
+		const systemCode = 'origami';
+
+		await createEntryFileSass(location, components, brand, systemCode);
+
+		const EntryFileContents = await fs.readFile(
+			path.join(location, 'index.scss'),
+			'utf-8'
+		);
+		proclaim.deepStrictEqual(
+			EntryFileContents,
+			dedent`
+                $o-brand: "master";
+                $system-code: "origami";
+
+                @import "@financial-times/o-grid/main";
+                @if not mixin-exists('oGrid') {
+                    @error 'Could not compile sass as @financial-times/o-grid does not have a primary mixin. ' +
+                    'If you think this is an issue, please contact the Origami community on Slack in #origami-support. ' +
+                    'If you want to learn more about what a primary mixin is, here is a link to the specification -- https://origami.ft.com/spec/v2/components/sass/#primary-mixin';
+                }
+                @include oGrid();
+                @import "@financial-times/o-table/main";
+                @if not mixin-exists('oTable') {
+                    @error 'Could not compile sass as @financial-times/o-table does not have a primary mixin. ' +
+                    'If you think this is an issue, please contact the Origami community on Slack in #origami-support. ' +
+                    'If you want to learn more about what a primary mixin is, here is a link to the specification -- https://origami.ft.com/spec/v2/components/sass/#primary-mixin';
+                }
+                @include oTable();`
+		);
+	});
+	it('does not import a component in the index.scss file if the component has no sass', async function () {
+		await fs.mkdir('/tmp/bundle/', {recursive: true});
+
+		const location = await fs.mkdtemp('/tmp/bundle/');
+
+		const components = {
+			'@financial-times/o-table': 'prerelease',
 			'@financial-times/o-grid': 'prerelease',
 			'preact': '^10.5.5'
 		};
