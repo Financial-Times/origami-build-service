fef1000ba2184a8dabebcf6ece38ff0f7d405b2f
Lee Moody <notlee@users.noreply.github.com>
2022-07-25 15:09:58 +0100

    feat!: Always use archive for OBS v2 demo pages. Remove demo building code.

--- test/integration/v2-archive.test.js
+++ test/integration/v2-archive.test.js
@@ -6,6 +6,135 @@ const {assert} = require('chai');
 describe('Archived routes', function() {
 	this.timeout(20000);
 	this.slow(5000);
+
+	describe('GET /__origami/service/build/v2/demos', function() {
+
+		describe('a request with archived response', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				response = await request(this.app)
+					.get('/__origami/service/build/v2/demos/o-header%406.14.5/header')
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 200 status', function() {
+				assert.equal(response.status, 200);
+			});
+
+			it('should respond with demo HTML', function() {
+				assert.include(response.headers['content-type'], 'html');
+			});
+		});
+
+		describe('a request with url-decoded query parameter, with an archived response for the url-encoded version', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				response = await request(this.app)
+					.get('/__origami/service/build/v2/demos/o-header@6.14.5/header')
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 200 status', function() {
+				assert.equal(response.status, 200);
+			});
+
+			it('should respond with demo HTML', function() {
+				assert.include(response.headers['content-type'], 'html');
+			});
+		});
+
+		describe('a request with no response saved in the archive', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				const now = (new Date()).toISOString();
+				response = await request(this.app)
+				// export used to cache bust, unrecognised params are removed by the archive
+					.get(`/__origami/service/build/v2/demos/o-header%5E6.14.5/header&export=${now.replace(/[^a-zA-Z0-9]/g, '')}`)
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 404 status', function() {
+				assert.equal(response.status, 404);
+			});
+		});
+	});
+
+	describe('GET /__origami/service/build/v2/demos/:fullModuleName/:demoName/html', function() {
+
+		describe('a request with archived response', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				response = await request(this.app)
+					.get('/__origami/service/build/v2/demos/o-header%406.14.5/header/html')
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 200 status', function() {
+				assert.equal(response.status, 200);
+			});
+
+			it('should respond with demo HTML as plain text', function() {
+				assert.include(response.headers['content-type'], 'text');
+			});
+		});
+
+		describe('a request with url-decoded query parameter, with an archived response for the url-encoded version', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				response = await request(this.app)
+					.get('/__origami/service/build/v2/demos/o-header@6.14.5/header/html')
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 200 status', function() {
+				assert.equal(response.status, 200);
+			});
+
+			it('should respond with demo HTML as plain text', function() {
+				assert.include(response.headers['content-type'], 'text');
+			});
+		});
+
+		describe('a request with no response saved in the archive', function() {
+			/**
+			* @type {request.Response}
+			*/
+			let response;
+			before(async function () {
+				const now = (new Date()).toISOString();
+				response = await request(this.app)
+				// export used to cache bust, unrecognised params are removed by the archive
+					.get(`/__origami/service/build/v2/demos/o-header%5E6.14.5/header&export=${now.replace(/[^a-zA-Z0-9]/g, '')}/html`)
+					.redirects(5)
+					.set('Connection', 'close');
+			});
+
+			it('should respond with a 404 status', function() {
+				assert.equal(response.status, 404);
+			});
+		});
+	});
+
 	// We cannot configure the env of a remote server to run tests
 	// which depend on a certain ARCHIVE env variable.
 	// Skip these tests on staging. The env is temporary whilst
