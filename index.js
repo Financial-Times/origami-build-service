e82e9d83fe25d4bc1824024e2828c678167b0684
Lee Moody <notlee@users.noreply.github.com>
2022-07-26 11:16:21 +0100

    backstage: Remove unused bower references from code
    No need to auth Github with user + password; no need to have bower envs; check npm rather than bower registry in health check; remove bower references outside the url updater migration tool

--- index.js
+++ index.js
@@ -17,25 +17,11 @@ const options = {
 	log: console,
 	metricsAppName: 'origami-build-service',
 	name: 'Origami Build Service',
-	bowerRegistryURL:
-		process.env.BOWER_REGISTRY_URL || 'http://origami-bower-registry.ft.com',
 	tempdir: `/tmp/buildservice-${process.pid}/`,
 	testHealthcheckFailure: process.env.TEST_HEALTHCHECK_FAILURE || false,
 	npmRegistryURL: process.env.NPM_REGISTRY_URL || 'https://registry.npmjs.org',
 	archiveBucketName: process.env.ARCHIVE_BUCKET_NAME || 'origami-build-service-archive-prod'
 };
-
-/**
- * This can be sync as it happens during application start-up.
- * The application shouldn't accept connections before it can access all Origami components.
- * This file is needed to access our private Origami components. E.G. o-fonts-assets
- * This code should probably live in moduleinstallation.js
- */
-mkdirp.sync(options.tempdir);
-const netrcFilePath = path.join(options.tempdir, '/.netrc');
-const netrcContents = `machine github.com\nlogin ${process.env.GITHUB_USERNAME}\npassword ${process.env.GITHUB_PASSWORD}`;
-fs.writeFileSync(netrcFilePath, netrcContents);
-process.env.HOME = options.tempdir; // Workaround: Bower ends up using $HOME/.local/share/bower/empty despite config overriding this
 
 buildService(options)
 	.listen()
