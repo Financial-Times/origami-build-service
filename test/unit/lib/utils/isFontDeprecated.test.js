03914dfe73a3af03555d5e06f8729a9471d468ea
Ben Freshwater <ben@frshwtr.uk>
2023-07-12 11:10:14 +0100

    or-236: deprecate fonts by explicit name (#616)

---
+++ test/unit/lib/utils/isFontDeprecated.test.js
@@ -0,0 +1,36 @@
+'use strict';
+const isFontDeprecated = require('../../../../lib/utils/isFontDeprecated');
+const assert = require('chai').assert;
+
+describe('isFontDeprecated', () => {
+	['MillerDisplay-Black', 'BentonSans-Bold', 'MillerDisplay-Regular'].forEach((font) => {
+		describe(`given a deprecated font: ${font}`, () => {
+			it('should return true', () => {
+				const result = isFontDeprecated(font);
+
+				assert.isTrue(result);
+			});
+		});
+	});
+
+	['MetricWeb', 'FinancierDisplayWeb'].forEach((font) => {
+		describe(`given a supported font:  ${font}`, () => {
+			it('should return false', () => {
+				const result = isFontDeprecated(font);
+
+				assert.isFalse(result);
+			});
+		});
+	});
+
+	['MillerDisplay_Givemeallyourpasswords', 'BentonSans_Hackyhack'].forEach((font) => {
+		describe(`given a font which partially matches a deprecated font:  ${font}`, () => {
+			it('should return false', () => {
+				const result = isFontDeprecated(font);
+
+				assert.isFalse(result);
+			});
+		});
+	});
+});
+
