978f06c2920eb6307f86597df5075416923f58a6
Akaki Mikaia <akakimikaia@gmail.com>
2024-06-19 12:01:42 +0100

    chore: add instance id on dev

--- opentelemetry.js
+++ opentelemetry.js
@@ -28,6 +28,7 @@ const resource = new Resource({
 	['heroku.app.id']: process.env.HEROKU_APP_ID,
 	['heroku.release.commit']: process.env.HEROKU_BUILD_COMMIT,
 	['heroku.release.creation_timestamp']: process.env.HEROKU_RELEASE_CREATED_AT,
+	['service.instance.id']: process.env.HEROKU_DYNO_ID,
 	[SEMRESATTRS_CLOUD_PROVIDER]: 'heroku',
 	[SEMRESATTRS_SERVICE_VERSION]: process.env.HEROKU_RELEASE_VERSION,
 });
