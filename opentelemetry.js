587aeffb441a3a2fbab80a04e5618ad009088245
Akaki Mikaia <akakimikaia@gmail.com>
2024-06-19 15:47:47 +0100

    chore: revert changes so instance id is randomly generated (#631)
    * chore: revert changes so instance id is randomly generated

--- opentelemetry.js
+++ opentelemetry.js
@@ -29,7 +29,6 @@ const resource = new Resource({
 	['heroku.app.id']: process.env.HEROKU_APP_ID,
 	['heroku.release.commit']: process.env.HEROKU_BUILD_COMMIT,
 	['heroku.release.creation_timestamp']: process.env.HEROKU_RELEASE_CREATED_AT,
-	['service.instance.id']: process.env.HEROKU_DYNO_ID,
 	[SEMRESATTRS_CLOUD_PROVIDER]: 'heroku',
 	[SEMRESATTRS_SERVICE_VERSION]: process.env.HEROKU_RELEASE_VERSION,
 	[SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV,
