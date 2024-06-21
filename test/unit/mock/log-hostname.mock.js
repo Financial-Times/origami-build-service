55444b218ffbed8aebadd5e5bad51a3a5d57e497
ben.freshwater <ben.freshwater@ft.com>
2024-06-21 10:51:31 +0100

    chore: upgrade sinon

--- test/unit/mock/log-hostname.mock.js
+++ test/unit/mock/log-hostname.mock.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const sinon = require('sinon');
-require('sinon-as-promised');
 
 const logHostname = module.exports = sinon.stub();
 const mockMiddleware = logHostname.mockMiddleware = sinon.spy();
--- test/unit/mock/sanitize-errors.mock.js
+++ test/unit/mock/sanitize-errors.mock.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const sinon = require('sinon');
-require('sinon-as-promised');
 
 const sanitizeErrors = module.exports = sinon.stub();
 const mockMiddleware = sanitizeErrors.mockMiddleware = sinon.spy();
